2b0f45e4c4cb50e9bb2583bb50af5191f6f977b8
giordi fungula <jodi.fungula@gmail.com>
2022-01-10 20:44:06 +0200

    updates to app.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,20 @@
 import React from 'react';
-import { Routes, Route, Link, Outlet } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Layout from 'Layouts';
 import AddOffice from 'Components/ManageOffice/AddOffice';
 import EditOffice from 'Components/ManageOffice/EditOffice';
 import LandingPage from 'Components/Home';
-import Office from 'Components/Office';
 import ViewOffice from 'Components/Office/Page';
-
-function Dashboard() {
-	return <h1>Dashboard</h1>;
-}
 
 function App() {
 	return (
 		<Routes>
 			<Route path="/" element={<Layout />}>
 				<Route index element={<LandingPage />} />
-				<Route path="/dashboard" element={<Dashboard />} />
 				<Route
 					path="add-staff"
 					element={<ViewOffice heading="Office" />}
-				>
-					<Route path=":officeId" element={<Dashboard />} />
-				</Route>
+				></Route>
 				<Route
 					path="/new-office"
 					element={<AddOffice heading="New Office" />}
